ba1a0d6f3b28e48bfdf61971b42cf8c7b63ac342
artificial-life <proteus.protocol@gmail.com>
2016-05-31 04:23:46 +0600

    import

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
-	EventRouter: require('./build/event-router.js'),
-	TaskListener: require('./build/worker.js'),
-	PubSubAdapter: require('./build/pubsub-adapter.js'),
-	Broker: require('./build/broker.js')
+  EventRouter: require('./build/event-router.js'),
+  TaskListener: require('./build/worker.js'),
+  PubSubAdapter: require('./build/pubsub-adapter.js'),
+  Broker: require('./build/broker.js'),
+  HybridBroker: require('./build/hybrid-broker.js')
 };
