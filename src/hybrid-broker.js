b258693cbef4db60f391290427cce0967e83f0b7
artificial-life <proteus.protocol@gmail.com>
2016-06-07 19:47:43 +0600

    upd

--- src/hybrid-broker.js
+++ src/hybrid-broker.js
@@ -8,45 +8,48 @@ let RequestPool = require('async-requests').RequestPool;
 const HEARTBEAT_INTERVAL = 2000;
 
 class HybridBroker extends Broker {
-  constructor(uri) {
-    super(uri);
-    this.pool = new RequestPool('cycle', 100000);
-    this.worker_status = {};
-    this.heartbeat_id = setInterval(() => {
-      this.heartbeat();
-    }, HEARTBEAT_INTERVAL);
-  }
-  heartbeat() {
-    _.forEach(this.worker_status, (status, worker_id) => {
-      this.addTask('heartbeat', '', worker_id).timeout(1000).catch((e) => this.onstoplistenTask(worker_id, {}));
-    });
-  }
-  onlistenTask(identity, payload) {
-    this.worker_status[identity] = true;
-    super.onlistenTask(identity, payload);
-  }
-  onstoplistenTask(identity, payload) {
-    !payload.body && _.unset(this.worker_status, identity); //@NOTE: removing worker from list
-    super.onstoplistenTask(identity, payload);
-  }
-  onresponseTask(identity, payload) {
-    payload._recipient != 'self' ? super.onresponseTask(identity, payload) : this.pool.handleRequest(payload);
-  }
-  addTask(taskname, params, target_worker) {
-    let request = this.pool.createRequest();
-    let payload = {
-      type: 'addTask',
-      request_id: request.id,
-      body: {
-        taskname,
-        params
-      }
-    };
-    //@NOTE: may be identity, not const?
-    this.onaddTask('self', payload, target_worker);
+	constructor(uri) {
+		super(uri);
+		this.pool = new RequestPool('cycle', 100000);
+		this.worker_status = {};
+		this.heartbeat_id = setInterval(() => {
+			this.heartbeat();
+		}, HEARTBEAT_INTERVAL);
+	}
+	getWorkersCount() {
+		return _.size(worker_status);
+	}
+	heartbeat() {
+		_.forEach(this.worker_status, (status, worker_id) => {
+			this.addTask('heartbeat', '', worker_id).timeout(1000).catch((e) => this.onstoplistenTask(worker_id, {}));
+		});
+	}
+	onlistenTask(identity, payload) {
+		this.worker_status[identity] = true;
+		super.onlistenTask(identity, payload);
+	}
+	onstoplistenTask(identity, payload) {
+		!payload.body && _.unset(this.worker_status, identity); //@NOTE: removing worker from list
+		super.onstoplistenTask(identity, payload);
+	}
+	onresponseTask(identity, payload) {
+		payload._recipient != 'self' ? super.onresponseTask(identity, payload) : this.pool.handleRequest(payload);
+	}
+	addTask(taskname, params, target_worker) {
+		let request = this.pool.createRequest();
+		let payload = {
+			type: 'addTask',
+			request_id: request.id,
+			body: {
+				taskname,
+				params
+			}
+		};
+		//@NOTE: may be identity, not const?
+		this.onaddTask('self', payload, target_worker);
 
-    return request.promise;
-  }
+		return request.promise;
+	}
 }
 
 module.exports = HybridBroker;
--- src/worker.js
+++ src/worker.js
@@ -5,92 +5,102 @@ let zmq = require('zmq');
 let RequestPool = require('async-requests').RequestPool;
 
 class Worker {
-  constructor(uri, name) {
-    this.worker = zmq.socket('dealer');
-    this.worker.identity = 'zmq-worker-' + (name || process.pid);
+	constructor(uri, name) {
+		this.worker = zmq.socket('dealer');
+		this.worker.identity = 'zmq-worker-' + (name || process.pid);
 
-    this.worker.on('connect', () => {
-      console.log('connected', process.pid);
-      this.listenTask('heartbeat', () => {
-        console.log('hb on', process.pid);
-        return true;
-      })
-    });
-    this.worker.monitor(500, 0);
-    this.worker.connect(uri);
+		this.callbacks = {};
+		this.listenTask('heartbeat', () => {
+			console.log('hb on', process.pid);
+			return true;
+		});
 
-    this.worker.on('message', (...args) => this.handleMessage(args));
-    this.pool = new RequestPool('cycle', 100000);
+		this.worker.on('connect', () => {
+			console.log('connected', process.pid);
+			let registered_tasks = _.keys(this.callbacks);
+			console.log('registering tasks', registered_tasks);
+			_.forEach(registered_tasks, (taskname) => {
+				this.send({
+					type: 'listenTask',
+					body: taskname
+				});
+			});
+		});
+		this.worker.monitor(500, 0);
+		this.worker.connect(uri);
 
-    this.callbacks = {};
-  }
-  handleResponse(data) {
-    this.pool.handleRequest(data);
-  }
-  makeResponse(recipient, request_id, data) {
-    this.send({
-      type: 'responseTask',
-      body: data,
-      _recipient: recipient,
-      request_id: request_id
-    });
-  }
-  handleMessage(args) {
-    let payload = JSON.parse(args[1]);
-    let {
-      body: {
-        taskname: taskname,
-        params: params,
-      },
-      type: type,
-      request_id: request_id,
+		this.worker.on('message', (...args) => this.handleMessage(args));
+		this.pool = new RequestPool('cycle', 100000);
 
-      _sender: sender
-    } = payload;
+	}
+	handleResponse(data) {
+		this.pool.handleRequest(data);
+	}
+	makeResponse(recipient, request_id, data) {
+		this.send({
+			type: 'responseTask',
+			body: data,
+			_recipient: recipient,
+			request_id: request_id
+		});
+	}
+	handleMessage(args) {
+		let payload = JSON.parse(args[1]);
+		let {
+			body: {
+				taskname: taskname,
+				params: params,
+			},
+			type: type,
+			request_id: request_id,
 
-    if (type == 'responseTask') {
-      return this.handleResponse(payload);
-    }
+			_sender: sender
+		} = payload;
 
-    let cb = this.callbacks[taskname];
-    if (!cb) return this.makeErrorResponse(sender, request_id);
-    let result = cb(params);
-    Promise.resolve(result).then((d) => this.makeResponse(sender, request_id, d));
-  }
-  makeErrorResponse() {
-    console.log('Everything bad');
-  }
-  send(message) {
-    this.worker.send(['', JSON.stringify(message)])
-  }
-  addTask(taskname, params) {
-    let request = this.pool.createRequest();
+		if (type == 'responseTask') {
+			return this.handleResponse(payload);
+		}
 
-    this.send({
-      type: 'addTask',
-      body: {
-        params,
-        taskname
-      },
-      request_id: request.id
-    });
+		let cb = this.callbacks[taskname];
+		if (!cb) return this.makeErrorResponse(taskname, sender, request_id);
 
-    return request.promise;
-  }
-  stoplistenTask(taskname) {
-    this.send({
-      type: 'stoplistenTask',
-      body: taskname
-    });
-    taskname && _.unset(this.callbacks, taskname);
-  }
-  listenTask(taskname, callback) {
-    this.send({
-      type: 'listenTask',
-      body: taskname
-    });
-    this.callbacks[taskname] = callback;
-  }
+		let result = cb(params);
+		Promise.resolve(result).then((d) => this.makeResponse(sender, request_id, d));
+	}
+	makeErrorResponse(taskname, sender, request_id) {
+		console.log('Not task', taskname);
+	}
+	send(message) {
+		this.worker.send(['', JSON.stringify(message)])
+	}
+	addTask(taskname, params) {
+		let request = this.pool.createRequest();
+
+		this.send({
+			type: 'addTask',
+			body: {
+				params,
+				taskname
+			},
+			request_id: request.id
+		});
+
+		return request.promise;
+	}
+	stoplistenTask(taskname) {
+		this.send({
+			type: 'stoplistenTask',
+			body: taskname
+		});
+		taskname && _.unset(this.callbacks, taskname);
+	}
+	listenTask(taskname, callback) {
+		this.send({
+			type: 'listenTask',
+			body: taskname
+		});
+		this.callbacks[taskname] = callback;
+	}
 }
 
 module.exports = Worker;
