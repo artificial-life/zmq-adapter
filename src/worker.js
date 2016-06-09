a34041247c8aa90328b959bcd6de7d47b60c88aa
artificial-life <proteus.protocol@gmail.com>
2016-06-10 05:47:53 +0600

    upd

--- src/worker.js
+++ src/worker.js
@@ -16,78 +16,72 @@ class Worker {
       console.log('connected', process.pid);
       let registered_tasks = _.keys(this.callbacks);
 
-		this.worker.on('message', (...args) => this.handleMessage(args));
-		this.pool = new RequestPool('cycle', 100000);
+      _.forEach(registered_tasks, (taskname) => {
+        this.send({
+          type: 'listenTask',
+          body: taskname
+        });
+      });
+    });
+    this.worker.monitor(500, 0);
+    this.worker.connect(uri);
 
-	}
-	handleResponse(data) {
-		this.pool.handleRequest(data);
-	}
-	makeResponse(recipient, request_id, data) {
-		this.send({
-			type: 'responseTask',
-			body: data,
-			_recipient: recipient,
-			request_id: request_id
-		});
-	}
-	handleMessage(args) {
-		let payload = JSON.parse(args[1]);
-		let {
-			body: {
-				taskname: taskname,
-				params: params,
-			},
-			type: type,
-			request_id: request_id,
+    this.worker.on('message', (...args) => this.handleMessage(args));
+    this.pool = new RequestPool('cycle', 100000);
 
-			_sender: sender
-		} = payload;
+  }
+  handleResponse(data) {
+    this.pool.handleRequest(data);
+  }
+  makeResponse(recipient, request_id, data) {
+    this.send({
+      type: 'responseTask',
+      body: data,
+      _recipient: recipient,
+      request_id: request_id
+    });
+  }
+  handleMessage(args) {
+    let payload = JSON.parse(args[1]);
+    let {
+      body: {
+        taskname: taskname,
+        params: params,
+      },
+      type: type,
+      request_id: request_id,
 
-		if (type == 'responseTask') {
-			return this.handleResponse(payload);
-		}
+      _sender: sender
+    } = payload;
 
-		let cb = this.callbacks[taskname];
-		if (!cb) return this.makeErrorResponse(taskname, sender, request_id);
+    if (type == 'responseTask') {
+      return this.handleResponse(payload);
+    }
 
-		let result = cb(params);
-		Promise.resolve(result).then((d) => this.makeResponse(sender, request_id, d));
-	}
-	makeErrorResponse(taskname, sender, request_id) {
-		console.log('Not task', taskname);
-	}
-	send(message) {
-		this.worker.send(['', JSON.stringify(message)])
-	}
-	addTask(taskname, params) {
-		let request = this.pool.createRequest();
+    let cb = this.callbacks[taskname];
+    if (!cb) return this.makeErrorResponse(taskname, sender, request_id);
 
-		this.send({
-			type: 'addTask',
-			body: {
-				params,
-				taskname
-			},
-			request_id: request.id
-		});
+    let result = cb(params);
+    Promise.resolve(result).then((d) => this.makeResponse(sender, request_id, d));
+  }
+  makeErrorResponse(taskname, sender, request_id) {
+    console.log('Not task', taskname);
+  }
+  send(message) {
+    this.worker.send(['', JSON.stringify(message)])
+  }
+  addTask(taskname, params) {
+    let request = this.pool.createRequest();
 
-		return request.promise;
-	}
-	stoplistenTask(taskname) {
-		this.send({
-			type: 'stoplistenTask',
-			body: taskname
-		});
-		taskname && _.unset(this.callbacks, taskname);
-	}
-	listenTask(taskname, callback) {
-		this.send({
-			type: 'listenTask',
-			body: taskname
-		});
-		this.callbacks[taskname] = callback;
-	}
+    this.send({
+      type: 'addTask',
+      body: {
+        params,
+        taskname
+      },
+      request_id: request.id
+    });
+
     return request.promise;
   }
   stoplistenTask(taskname) {
